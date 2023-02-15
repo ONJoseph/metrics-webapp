fbbdd75546127496ab0a9ed2e6e32987e6854d91
ONJoseph <on.josephinfo@gmail.com>
2023-02-15 21:16:41 +0100

    Add home and details page to SPA

---
+++ src/Pages/DetailsPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ContinentsList from '../Components/ContinentsList';
+
+const Details = () => (
+  <div className="container">
+    <ContinentsList />
+  </div>
+);
+
+export default Details;
